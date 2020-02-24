3cc07facbba90497935e23823a035ebe993b5111
DracoMindz <DracoMindz@github.com>
2020-02-24 07:38:06 +0000

    minimum sum of factors

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python 3
+"""Caluculate minimum number of operations to create string"""
+
+import math
+from math import sqrt
+
+
+def minOperations(n):
+    """Find minimum sum of factors of number"""
+    numOps = 0
+    m = 2
+
+    if n < 2:
+        return 0
+    for m in range(2, int(sqrt(n) + 1)):
+        if (n % m == 0):
+            numOps += m
+            n /= m
+        m += 1
+    numOps += int(n)
+    return numOps
