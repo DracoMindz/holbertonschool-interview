a560ee1dcecab1a0178348c5c3c7730ba8ff9d4a
DracoMindz <DracoMindz@github.com>
2020-02-25 04:47:21 +0000

    sum factors

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -15,12 +15,9 @@ def minOperations(n):
         return 0
     for m in range(2, int(sqrt(n) + 1)):
         if (n % m == 0):
-            numOps += m
             n /= m
-            if n <= 1:
-                break
-
-        if (n % m != 0):
+            numOps = numOps + m
+        else:
             m += 1
     numOps += int(n)
     return numOps
