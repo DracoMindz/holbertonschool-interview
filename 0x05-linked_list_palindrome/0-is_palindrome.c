27172fd883fbe1fdca518f30070e9f1a79b152a9
DracoMindz <DracoMindz@github.com>
2020-03-12 07:21:12 +0000

    palindrome

---
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+/**
+ * is_palindrome - singly linked list
+ * @head: address of pointer
+ *
+ * Return: 1 if palindrome, 0 i f not palindrome
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *temp = *head;
+
+	return (got_pali(*head, &temp));
+
+}
+/**
+ * got_pali - reverse checks if is palindrome
+ * @head: address of pointer
+ * @nnode: half to compare
+ * Return: 1 if palindrome, 0 i f not palindrome
+ */
+int got_pali(listint_t *nnode, listint_t **head)
+{
+	if (!nnode)
+		return (1);
+	if ((!got_pali(nnode->next, head)))
+		return (1);
+	
+	if
+		((nnode->n == (*head)->n) && (*head = (*head)->next))
+		return (1);
+
+	return (0);
+}
---
+++ 0x05-linked_list_palindrome/README.md
@@ -0,0 +1,4 @@
+# 0x05. Linked list palindrome
+
+
+Write a function in C that checks if a singly linked list is a palindrome.
