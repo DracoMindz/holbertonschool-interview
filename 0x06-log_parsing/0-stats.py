03cedaeb2f7dcedd2d9de22163b2c9dca95624b3
DracoMindz <DracoMindz@github.com>
2020-03-18 17:36:02 +0000

    log parsing

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -5,44 +5,44 @@ Script reads stdin line by line and computes metrics
 
 import sys
 
-totalSize = [0]
-status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-counter = 0
+if __name__ == "__main__":
+    totalSize = [0]
+    status_codes = {200: 0, 301: 0, 400: 0,
+                    401: 0, 403: 0, 404: 0,
+                    405: 0, 500: 0}
+    counter = 0
 
+    def code_match(line):
+        """
+        match code to Status_codes
+        """
+        try:
+            line = line[:-1]
+            lineWords = line.split(" ")
+            totalSize[0] += int(lineWords[-1])
+            codenum = int(lineWords[-2])
+            if codenum in status_codes:
+                status_codes[codenum] += 1
+        except Exception:
+            pass
 
-def code_match(line):
-    """
-    match code to Status_codes
-    """
+    def print_stats():
+        """
+        print stats
+        """
+        print("Total file size: {}".format(totalSize[0]))
+        for m in sorted(status_codes.keys()):
+            if status_codes[m] != 0:
+                print("{}: {}".format(m, status_codes[m]))
+
     try:
-        line = line[:-1]
-        lineWords = line.split(" ")
-        totalSize[0] += int(lineWords[-1])
-        codenum = int(lineWords[-2])
-        if codenum in status_codes:
-            status_codes[codenum] += 1
+        for line in sys.stdin:
+            code_match(line)
+            if counter % 10 == 0:
+                print_stats()
+            counter += 1
     except Exception:
-        pass
+        print_stats()
+        raise
 
-
-def print_stats():
-    """
-    print stats
-    """
-    print("Total file size: {}".format(totalSize[0]))
-    for m in sorted(status_codes.keys()):
-        if status_codes[m] != 0:
-            print("{}: {}".format(m, status_codes[m]))
-
-
-try:
-    for line in sys.stdin:
-        code_match(line)
-        if counter % 10 == 0:
-            print_stats()
-        counter += 1
-except Exception:
     print_stats()
-    raise
-
-print_stats()
---
+++ 0x06-log_parsing/README.md
@@ -0,0 +1,8 @@
+# 0x06. Log Parsing
+Specializations - Interview Preparation ― Algorithms
+
+
+### ***Tasks***
+
+***0. Log parsing mandatory***
+Write a script that reads stdin line by line and computes metrics
