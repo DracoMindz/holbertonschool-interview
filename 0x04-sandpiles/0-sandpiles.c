e67f5e8d278d7bac05740988aa605417062fb6be
DracoMindz <DracoMindz@github.com>
2020-04-13 00:45:18 +0000

    sandpiles

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -3,14 +3,46 @@
 #include "sandpiles.h"
 
 /**
+ * stable_pile - check for stable sand piles
+ * @grid: 3 by 3 grid, stable
+ * Return: void
+ */
+int stable_pile(int grid[3][3])
+{
+	int i, j;
+
+	for (i = 0; i < 3; i++)
+		for (j = 0; j < 3; j++)
+			if (grid[i][j] > 3)
+				return (1);
+	return (0);
+}
+
+/**
+ * add_piles- add grids
+ * @grid1: 3 by 3 grid, stable
+ * @grid2: 3 by 3 grid, stable
+ * Return: void function
+ */
+void add_piles(int grid1[3][3], int grid2[3][3])
+{
+	int i, j;
+
+	for (i = 0; i < 3; i++)
+		for (j = 0; j < 3; j++)
+			grid1[i][j] = grid1[i][j] + grid2[i][j];
+}
+
+/**
  * print_grid - printd grid
  * @grid: 3 by 3 grid, stable
  * Return: void
  */
-static void print_grid(int grid[3][3])
+void print_grid(int grid[3][3])
 {
 	int i, j;
 
+	printf("=\n");
 	for (i = 0; i < 3; i++)
 	{
 		for (j = 0; j < 3; j++)
@@ -22,6 +54,42 @@ static void print_grid(int grid[3][3])
 		printf("\n");
 	}
 }
+
+/**
+ * dist_sand - tumble sand piles
+ * @grid1: 3 by 3 grid, stable
+ * @grid2: 3 by 3 grid, stable
+ * Return: void
+ */
+void dist_sand(int grid1[3][3], int grid2[3][3])
+{
+	int i, j;
+
+
+	for (i = 0; i < 3; i++)
+		for (j = 0; j < 3; j++)
+			grid2[i][j] = 0;
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (grid1[i][j] > 3)
+			{
+				grid2[i][j] -= 4;
+				if (i > 0)
+					grid2[i - 1][j] += 1;
+				if (i < 2)
+					grid2[i + 1][j] += 1;
+				if (j > 0)
+					grid2[i][j - 1] += 1;
+				if (j < 2)
+					grid2[i][j + 1] += 1;
+			}
+		}
+	}
+	add_piles(grid1, grid2);
+}
+
 /**
  * sandpiles_sum - computes sum of two grids
  * @grid1: 3 by 3 grid, stable
@@ -30,43 +98,10 @@ static void print_grid(int grid[3][3])
  */
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
-	int i, j, m;
-	int flag = 1;
-
-	for (i = 0; i <= 2; i++)
+	add_piles(grid1, grid2);
+	while (stable_pile(grid1))
 	{
-		for (j = 0; j <= 2; j++)
-			grid1[i][j] = grid1[i][j] + grid2[i][j];
-	}
-	printf("=\n");
-	print_grid(grid1);
-	while (flag == 1)
-	{
-		flag = 0;
-		for (i = 0; i < 3; i++)
-		{
-			for (j = 0; j < 3; j++)
-			{
-				m = grid1[i][j];
-				if (m >= 4)
-				{
-					flag = 1;
-					grid1[i][j] = m - 4;
-					if (i > 0)
-						grid1[i - 1][j] = grid1[i - 1][j] + 1;
-					if (i < 2)
-						grid1[i + 1][j] = grid1[i + 1][j] + 1;
-					if (j > 0)
-						grid1[i][j - 1] = grid1[i][j - 1] + 1;
-					if (j < 2)
-						grid1[i][j + 1] = grid1[i][j + 1] + 1;
-				}
-			}
-		}
-		if (flag == 1)
-		{
-			printf("=\n");
-			print_grid(grid1);
-		}
+		print_grid(grid1);
+		dist_sand(grid1, grid2);
 	}
 }
--- 0x04-sandpiles/sandpiles.h
+++ 0x04-sandpiles/sandpiles.h
@@ -2,6 +2,7 @@
 #define SANDPILES_H
 
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
-
+int stable_pile(int grid[3][3]);
+void add_piles(int grid1[3][3], int grid2[3][3]);
 
 #endif /*SANDPILES_H*/
