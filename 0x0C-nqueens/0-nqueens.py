d431afb214873d0dfe39dd2137add4a6d6176dc1
DracoMindz <43617819+DracoMindz@users.noreply.github.com>
2020-05-12 16:22:45 -0400

    N Queens

---
+++ 0x0C-nqueens/0-nqueens.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env pyhton3
+"""
+N queens puzzle challenge
+"""
+
+import sys
+
+
+if __name__ == "__main__":
+    """
+    N is number of Queens to be placed
+    NxN are dimeanions for chess board
+    exit and null cases
+    """
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        exit(1)
+
+    # N value introduced using try except
+    try:
+        N = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        exit(1)
+    if N < 4:
+        print("N must be at least 4")
+        exit(1)
+
+    # chess board, shape(N, N)
+    board = [[0 for j in range(N)]
+             for i in range(N)]
+    bestPlace(board, 0, N)
+
+
+def solutionprint(board):
+    """
+    print solution to NQueens
+    :param board: matrix, list of lists
+    :return: solution
+    """
+    solNQ = []
+    if i in range(len(board)):
+        for j in range(len(board)):
+            if board[1][j] == 1:
+                solNQ.append([i, j])
+    print(solNQ)
+
+
+def safeSpot(board, row, col, N):
+    """
+    Check if Queen is in  safe space
+    """
+    # check row on left side
+    for idx_i in range(col):
+        if board[row][idx_i] == 1:
+            return False
+
+    # check diagnol lower
+    for idx_i, idx_j in zip(range(row, N, -1), range(col, -1, -1)):
+        if board[idx_i][idx_j] == 1:
+            return False
+
+    # check diagonal upper
+    for idx_i, idx_j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[idx_i][idx_j] == 1:
+            return False
+    return True
+
+
+def bestPlace(board, col, N):
+    """
+    if all queens are placed, best place to place
+    :param board: list of lists
+    :param col: possible location
+    :return: true or false
+    """
+
+    if col == N:
+        solutionprint(board)
+        return True
+
+    # if is a safe Spot place the queen
+    safe = False
+    for i in range(N):
+        if safeSpot(board, index, col, N):
+            board[index][col] = 1
+            safe = bestPlace(board, col + 1, N) or safe
+
+            # no safe place remove Queen from board
+            board[index][col] = 0
+    return safe
---
+++ 0x0C-nqueens/README.md
@@ -0,0 +1,15 @@
+# 0x0C. N Queens
+Specializatinos - Interview Preparation - Algorithms
+
+### Tasks
+
+#### ***0. N queens***
+```
+The N queens puzzle is the challenge of placing N non-attacking queens on an N×N chessboard. 
+Write a program that solves the N queens problem.
+```
+
+Usage: nqueens N
+Include: Where N must be an integer greater or equal to 4
+The program should print every possible solution to the problem
+You are only allowed to import the sys module
