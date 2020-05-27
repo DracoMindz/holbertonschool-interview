88cf34db14452140dd8916c15b4661710e52f07a
DracoMindz <43617819+DracoMindz@users.noreply.github.com>
2020-05-27 18:12:07 -0400

    added files

--- 0x0E-linear_skip/search.h
+++ 0x0E-linear_skip/search.h
@@ -25,5 +25,6 @@ skiplist_t *create_skiplist(int *array, size_t size);
 void free_skiplist(skiplist_t *list);
 void print_skiplist(const skiplist_t *list);
 skiplist_t *linear_skip(skiplist_t *list, int value);
+skiplist_t *print_val(skiplist_t *prevskip, skiplist_t *expskip, int value)
 
 #endif
