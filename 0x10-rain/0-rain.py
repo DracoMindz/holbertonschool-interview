b26be3b667aff3900c5e87880167994669798ba9
DracoMindz <43617819+DracoMindz@users.noreply.github.com>
2020-06-10 20:15:16 -0400

    made pythonic changes

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -13,39 +13,14 @@ def rain(walls):
 
     if len(walls) == 0:
         return 0
-    waterHeld = 0   # total amount of water retained
-    stackRight = []   # height of tallest bar on right
-    stackLeft = []   # Height of tallest bart on the left
+    waterRetained = 0   # total amount of water retained
+    stackRight = 0   # height of tallest bar on right
+    stackLeft = 0  # Height of tallest bart on the left
 
-    # NUll case left side
-    for idx, height in enumerate(walls):
-        if not stackLeft or height > waterHeld:
-            stackLeft.append(idx)
-            waterHeld = height
+    for height in range(1, len(walls) - 1):
+        stackLeft = max(walls[:height])
+        stackRight = max(walls[height+1:])
 
-    waterHeld = 0   # Null case for right side (in reverse)
-    for idx, height in enumerate(walls[::-1], 1):
-        if not stackrighyt or height > waterHeld:
-            stackLeft.append(-idx)
-            waterHeld = height
-
-    # define the waterHeld it contains height left, right
-    # define waterHeld for the right side
-    for indxH, indxW in enumerate(stackRight[:-1]):
-        # water += distance *(max stack - height @ idx
-        stackRight = (-stackRight[indxH + 1] + indxW)
-        waterHeld += walls[indxW] * stackRight
-
-    # define the waterHeld for the leftside
-    for indxH, indxW in enumerate(stackLeft[:-1]):
-        # water += distance *(max stack - height @ idx
-        stackLeft = (stackLeft[indxH + 1] - indxW)
-        waterHeld += walls[indxW] * stackLeft
-
-    heldLength = ((len(walls) + 1 + stackRight[-1] - stackLeft[-1])
-    heldWidth == (walls[stackLeft[-1]])
-    # define waterHeld possible length times possible width
-    waterHeld += heldLength * heldWidth
-
-    waterRetained = waterHeld - sum(walls)
+        reservoir = min(stackLeft, stackRight) - walls[height]
+        waterRetained += max(0, reservoir)
     return waterRetained
