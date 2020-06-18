d907cd4c9ac79da8682f28c816e75af2ad482f67
DracoMindz <43617819+DracoMindz@users.noreply.github.com>
2020-06-17 23:18:13 -0400

    Heap Sort

---
+++ 0x11-heap_sort/0-heap_sort.c
@@ -0,0 +1,59 @@
+#include "sort.h"
+
+/**
+ * sift_down - sorts an array of integers in ascending order
+ * @array: list to be sorted
+ * @size: length of array
+ * @root: num
+ *
+ * Return: Always 0
+ */
+void sift_down(int *array, size_t size, size_t root)
+{
+	int interim = 0;
+	int key;
+
+	size_t bignum = root * 2;
+
+	if ((bignum <= size) && (array[root - 1] < array[bignum - 1]))
+		interim = bignum - 1;
+	if (bignum + 1 <= size && array[root - 1] < array[bignum]
+		&& array[bignum - 1] < array[bignum])
+		interim = bignum;
+	/* swap algorithm */
+	if (interim)
+	{
+		key = array[interim];
+		array[interim] = array[root - 1];
+		array[root - 1] = key;
+		/* print after each swap */
+		print_array(array, size);
+	}
+}
+
+/**
+ * heap_sort - sorts an array of integers in ascending order
+ * @array: list to be sorted
+ * @size: length of array
+ *
+ * Return: Always 0
+ */
+void heap_sort(int *array, size_t size)
+{
+	size_t idx;
+	int temp;
+	size_t root = 0;
+
+	for (idx = (size - 2) / 2; idx >= 0; idx--)
+	{
+		sift_down(array, size, root);
+	}
+	for (idx = 0; idx < size; idx++)
+	{
+		temp = array[size - idx - 1];
+		array[size - idx - 1] = array[0];
+		array[0] = temp;
+		print_array(array, size);
+		sift_down(array, size - idx - 1, 1);
+	}
+}
---
+++ 0x11-heap_sort/README.md
@@ -0,0 +1,36 @@
+# 0x11. Heap Sort
+Specialization - Interview Preparation - Algorithms
+
+Create a header file: sort.h.
+
+Include in header file:
+```
+void print_array(const int *array, size_t size)
+```
+
+Create file 0-O:
+```
+Using big O notation to explain the time complexity:
+
+Use this form of nig O notation
+
+O(1)
+O(n)
+O(n!)
+n square -> O(n^2)
+log(n) -> O(log(n))
+n * log(n) -> O(nlog(n))
+
+```
+
+## Task Question
+***
+**0. Heap sort**
+```
+Write a function that sorts an array of integers in
+ascending order using the Heap sort algorithm.
+
+Prototype: void heap_sort(int *array, size_t size);
+You must implement the sift-down heap sort algorithm
+```
+
