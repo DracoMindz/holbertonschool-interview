8ccff9ec24948c125c05d8ae703ad4af7b0213ca
DracoMindz <43617819+DracoMindz@users.noreply.github.com>
2020-06-25 00:40:09 -0400

    binary

--- 0x12-advanced_binary_search/0-advanced_binary.c
+++ 0x12-advanced_binary_search/0-advanced_binary.c
@@ -2,30 +2,27 @@
 #include <stdlib.h>
 #include "search_algos.h"
 
+
 /**
- * advanced_binary - Entry point
+ * search - Entry point
  * @array: pointer to first element of array
  * @size: num of elements im array
  * @value: value to search for
  *
  * Return: Always EXIT_SUCCESS
  */
-int advanced_binary(int *array, size_t size, int value)
+int search_list(int *array, size_t size, int value)
 {
+	size_t iter;
 	size_t left;
 	size_t right;
-	size_t  iter;
-	int *location
 
 	left = (size / 2);
 	right = (size / 2 - 1);
 
-	if (array == 0)
-		return (-1);
-	if (size < 0)
-		return (-1);
+
 	if (size == 1 && array[0] == value)
-		return (array);
+		return (array[0]);
 	printf("Searching in array: %d", array[0]);
 	/* print loop */
 	for (iter = 1; iter < size; iter++)
@@ -36,10 +33,35 @@ int advanced_binary(int *array, size_t size, int value)
 		{
 		if (array[right] >= value)
 			return (advanced_binary(array, left + size % 2, value));
-
 		else
 			return (advanced_binary(array + left + size % 2, left, value));
 		}
+	return (-1);
+}
+
+
+/**
+ * advanced_binary - Entry point
+ * @array: pointer to first element of array
+ * @size: num of elements im array
+ * @value: value to search for
+ *
+ * Return: Always EXIT_SUCCESS
+ */
+int advanced_binary(int *array, size_t size, int value)
+{
+
+	/*int *val_address;*/
+
+
+	if (array == 0)
+		return (-1);
+	if (size <= 0)
+		return (-1);
+
+	/*val_address = search_list(array, size, value);*/
 	/* if element not present in the array*/
-	return -1
+	/*if (val_address == NULL)*/
+		/*return (-1);*/
+	return (search_list(array, size, value));
 }
