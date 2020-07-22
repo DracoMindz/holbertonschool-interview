480eb701a32b3086fea50e21316849106a048245
DracoMindz <43617819+DracoMindz@users.noreply.github.com>
2020-07-22 03:45:02 +0000

    rotate rt 90 update len description

--- 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -21,7 +21,7 @@ def rotate_2d_matrix(matrix):
             matrix[lenMat - c - 1][r] = matrix[lenMat - r - 1][lenMat - c - 1]
             matrix[lenMat - r - 1][lenMat - c - 1] = matrix[c][lenMat - r - 1]
             matrix[c][lenMat - r - 1] = tempMatrix
-    if lenMat == 2:
+    if len(matrix) == 2:
         tempMatrix = matrix[0][0]
         matrix[0][0] = matrix[1][0]
         matrix[1][0] = matrix[1][1]
