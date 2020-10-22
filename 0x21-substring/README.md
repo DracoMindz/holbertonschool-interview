329ac22353af9bf0423e4d7d1a5f660267e5c103
DracoMindz <43617819+DracoMindz@users.noreply.github.com>
2020-10-22 03:05:29 +0000

    substring

---
+++ 0x21-substring/README.md
@@ -0,0 +1,14 @@
+# 0x21 Substring with Concatenated Words
+
+Specializations - Interview Preparation - Algorithms
+
+
+## Tasks
+
+**0. Substring with concatenated words**
+
+---
+
+Write a function that finds all the possible substrings containing a list of words, within a given string.
+
+---
---
+++ 0x21-substring/substring.c
@@ -0,0 +1,57 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "substring.h"
+
+
+/**
+ * find_substring - find all possible holderstrings containing a list of words
+ * @s: the string to scan
+ * @words: the array of words all holderstrings must be a concatenation of
+ * @nb_words: the num of elements in the array words
+ * @n: holds the address at which to store the num of lelements in array
+ * Return: array storing each index in s at which holderstring is found or NULL
+ */
+int *find_substring(char const *s, char const **words, int nb_words, int *n)
+{
+	char *holder = NULL, *checkstr = NULL;
+	int *string, stringlen, dict = 0, count = 0, i, j, k;
+
+	stringlen = strlen(s);
+	dict = strlen(words[0]) * nb_words;
+	string = malloc(stringlen + 1);
+	holder = malloc(dict + 1);
+	for (i = 0; i < stringlen; i++)
+	{
+		for (j = 0; j < nb_words; j++)
+		{
+			for (k = 0; k < dict;)
+			{
+				checkstr = strstr(holder + k, words[j]);
+				if (checkstr != NULL)
+				{
+					if ((checkstr - holder) % (strlen(words[0])) == 0)
+						break;
+					if ((checkstr - holder) % (strlen(words[0])) != 0)
+					{
+						k = checkstr - holder + 1;
+						checkstr = NULL;
+					}
+				}
+				else if (checkstr == NULL)
+					continue;
+			}
+			if (checkstr != NULL)
+				memcpy(&checkstr[i], &holder, sizeof(holder));
+			else
+				continue;
+		}
+		if (stringlen != 0 && dict != 0)
+			string[count++] = i;
+		i++;
+	}
+	free(holder);
+	*n = count;
+
+	return (string);
+}
